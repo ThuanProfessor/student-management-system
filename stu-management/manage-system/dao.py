e24067c92f72d135533a410eb14f9b39ab25fc05
HoangThuanDev <beanheo2014@gmail.com>
2024-12-14 14:33:31 +0700

    BTL

--- stu-management/manage-system/dao.py
+++ stu-management/manage-system/dao.py
@@ -6,3 +6,22 @@ def load_student_list():
         "id": 2,
         "name": "Nguyễn Trung Hậu"
     }]
+
+
+def load_category():
+    return [{
+        "id": 1,
+        "name": "Trang Chủ"
+    }, {
+        "id": 2,
+        "name": "Tiếp Nhận Học Sinh"
+    }, {
+        "id": 3,
+        "name": "Nhập Điểm"
+    }, {
+        "id": 4,
+        "name": "Xuất Điểm"
+    }, {
+        "id": 5,
+        "name": "Lập Báo Cáo"
+    }]
